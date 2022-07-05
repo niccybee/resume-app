fc1d6100626a2e369b301bcbe5ca37379a0904b3
niccybee <niccybee@users.noreply.github.com>
2022-07-05 22:50:03 +1000

    cleaning continues

--- src/App.vue
+++ src/App.vue
@@ -2,19 +2,12 @@
 import { ref } from "vue";
 import AppHeader from "./components/AppHeader.vue";
 import AppBody from "./components/AppBody.vue";
-
-const showCreateBox = ref(false);
-let handleShowCreate = () => {
-  console.log("caught create show event");
-  showCreateBox.value = true;
-};
-let showT;
 </script>
 
 <template>
   <div class="container">
-    <AppHeader @showCreateEvent="handleShowCreate" />
-    <AppBody :createItemModal="showCreateBox" />
+    <AppHeader />
+    <AppBody />
   </div>
 </template>
 

5285d60ede7feb85a11cc3eb46e1c71710c05fb4
niccybee <niccybee@users.noreply.github.com>
2022-09-08 11:50:23 +1000

    state

--- src/main.js
+++ src/main.js
@@ -1,6 +1,18 @@
 // src/main.ts
 import { ViteSSG } from "vite-ssg/single-page";
 import App from "./App.vue";
+import { createPinia } from "pinia";
+import { useCvStore } from "./stores/cvStore";
 
 // `export const createApp` is required instead of the original `createApp(App).mount('#app')`
-export const createApp = ViteSSG(App);
+export const createApp = ViteSSG(App, ({ app, intiialState }) => {
+  const pinia = createPinia();
+  app.use(pinia);
+  const cvs = useCvStore(pinia);
+  if (!cvs.ready) cvs.getCVs();
+  if (import.meta.env.SSR) {
+    intialState.pinia = pinia.state.value;
+  } else {
+    console.log(intiialState);
+  }
+});
